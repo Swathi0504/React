62ef90b052bf0f66dcb16c160bf2adef980d338b
Swathi0504 <133556424+Swathi0504@users.noreply.github.com>
2024-03-28 12:48:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [
+        React.createElement(
+            "h1",
+            {},
+            "I am a h1 tag"
+        ),
+        React.createElement(
+            "h1",
+            {},
+            "React App"
+        )
+        ]
+    )
+);
+
+
+
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello world from react");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
